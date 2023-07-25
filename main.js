5859e8ffb74e870d4a8062bcd9e0a42452e1ecea
GARROFE Matias <alanmatia@gmail.com>
2023-07-25 00:32:56 -0300

    en preceso

---
+++ main.js
@@ -0,0 +1,15 @@
+let typed = new Typed(".text", {
+    strings:["Frontend Developer" , "Analista De Sístemas Informaticos" , "Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true 
+});
+
+let typedd = new Typed(".contacto", {
+    strings:["Contactame" , "escribime!"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 1000,
+    loop:true 
+});
